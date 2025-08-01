2e0b66772c86e82f5761eb60f2797726218c6b28
Maria Micaela Vidal <131964698+mikividal@users.noreply.github.com>
2025-08-01 09:39:16 +0100

    javascript bug fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,14 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "controllers"
+// // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// import "controllers"
+// import "@hotwired/turbo-rails"
+// import "@popperjs/core"
+// import "bootstrap"
+// console.error("🌐 Controladores importados correctamente")
+
+console.log("application.js se está cargando...")
 import "@hotwired/turbo-rails"
-import "@popperjs/core"
+console.log("turbo cargado")
+import "controllers"
+console.log("controllers importado")
 import "bootstrap"
-console.error("🌐 Controladores importados correctamente")
+console.log("bootstrap importado")
